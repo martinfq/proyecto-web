d82e6cfecc42a129cb66429bbaf9038442ab44d5
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2023-09-01 18:54:20 -0500

    funciones para la sesion y cuentas

---
+++ frontend/src/js/main.js
@@ -0,0 +1,16 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js"
+import { auth } from './firebase.js'
+import {loginCheck} from './loginCheck.js'
+import { showMessage } from './showMessage.js'
+
+
+
+import './logout.js'
+onAuthStateChanged(auth, async (user) => {
+    if ( user ) {
+        //showMessage("Bienvenido: " + user.email, 'success');
+        loginCheck(user)
+    } else {
+        loginCheck(user)
+    }
+})
